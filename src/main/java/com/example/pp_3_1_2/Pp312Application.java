364c729d0a343199dc75560f6d4f7b9aafbc2de8
glowlotus <glowlotos@gmail.com>
2023-08-05 15:12:26 +0300

    C3 - то ли лыжи не едут то ли я ебанутый

--- src/main/java/com/example/pp_3_1_2/Pp312Application.java
+++ src/main/java/com/example/pp_3_1_2/Pp312Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.pp_3_1_2.dao")
-@EntityScan(basePackages = "com.example.pp_3_1_2.model")
 public class Pp312Application {
 
     public static void main(String[] args) {
